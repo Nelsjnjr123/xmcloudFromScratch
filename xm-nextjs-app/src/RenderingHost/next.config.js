330939a0109db0a742329b2870336d94f8dd947e
Nelson Joel Raj <nraj@farahexperiences.com>
2023-03-09 15:53:08 +0400

    f

--- xm-nextjs-app/src/RenderingHost/next.config.js
+++ xm-nextjs-app/src/RenderingHost/next.config.js
@@ -19,7 +19,11 @@ const nextConfig = {
   env: {
     PUBLIC_URL: publicUrl,
   },
-
+    eslint: {
+        // Warning: This allows production builds to successfully complete even if
+        // your project has ESLint errors.
+        ignoreDuringBuilds: true,
+    },
   i18n: {
     // These are all the locales you want to support in your application.
     // These should generally match (or at least be a subset of) those in Sitecore.
@@ -55,11 +59,7 @@ const nextConfig = {
 };
 
 module.exports = () => {
-    eslint: {
-        // Warning: This allows production builds to successfully complete even if
-        // your project has ESLint errors.
-        ignoreDuringBuilds: true,
-  }
+   
   // Run the base config through any configured plugins
   return Object.values(plugins).reduce((acc, plugin) => plugin(acc), nextConfig);
 }
