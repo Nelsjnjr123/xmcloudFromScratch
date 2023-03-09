473d68e7e55154645d182c589be912c4f8af3579
Nelson Joel Raj <nraj@farahexperiences.com>
2023-03-09 15:35:08 +0400

    d

--- xm-nextjs-app/src/RenderingHost/src/lib/page-props-factory/plugins/personalize.ts
+++ xm-nextjs-app/src/RenderingHost/src/lib/page-props-factory/plugins/personalize.ts
@@ -2,7 +2,7 @@ import { GetServerSidePropsContext, GetStaticPropsContext } from 'next';
 import { Plugin } from '..';
 import { getPersonalizedRewriteData, personalizeLayout } from '@sitecore-jss/sitecore-jss-nextjs';
 import { SitecorePageProps } from 'lib/page-props';
-import {ComponentRendering} from  '@sitecore-jss/sitecore-jss-nextjs';
+
 class PersonalizePlugin implements Plugin {
   order = 2;
 
@@ -18,22 +18,7 @@ class PersonalizePlugin implements Plugin {
 
     // Get variant for personalization (from path)
     const personalizeData = getPersonalizedRewriteData(path);
-    //personalizeData.
-    console.log(`${personalizeData.variantId}xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx`);
-  
-    console.log(`${props.layoutData.sitecore.route?.placeholders["headless-main"].map(
-      function(i){ 
-        if(i as ComponentRendering)
-       {
-          let a=i as ComponentRendering
-           if(a.fields !==undefined)
-           {
-            a.fields
-           }
-       }
-      }
-
-    )} nnnnnnnnnnnnnnxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx`);
+   
     // Modify layoutData to use specific variant instead of default
     // This will also set the variantId on the Sitecore context so that it is accessible here
       personalizeLayout(props.layoutData, personalizeData.variantId);
